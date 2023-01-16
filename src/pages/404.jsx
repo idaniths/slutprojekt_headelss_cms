68e78c4a939a3d58ddc62794ebcda45d0c578c5b
idaniths <idan.cheaproot@iths.se>
2023-01-16 16:53:26 +0100

    -

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,39 +3,41 @@ import { Link } from "gatsby";
 
 const NotFoundPage = () => {
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        backgroundColor: "rgb(37, 34, 34)",
-      }}
-    >
-      <h1
+    <>
+      <main
         style={{
-          fontSize: "8rem",
-          background: "linear-gradient(80deg, #ee0000 0%, #ff8c00 100%)",
-          WebkitBackgroundClip: "text",
-          WebkitTextFillColor: "transparent",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          backgroundColor: "rgb(37, 34, 34)",
         }}
       >
-        404
-      </h1>
-      <h1 style={{ color: "white" }}>Page not found</h1>
-      <Link
-        to="/"
-        style={{
-          color: "rgb(202, 0, 0)",
-          textDecoration: "none",
-          fontSize: "1.8rem",
-          marginTop: "1rem",
-        }}
-      >
-        Go back to home
-      </Link>
-    </main>
+        <h1
+          style={{
+            fontSize: "8rem",
+            background: "linear-gradient(80deg, #ee0000 0%, #ff8c00 100%)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          404
+        </h1>
+        <h1 style={{ color: "white" }}>Page not found</h1>
+        <Link
+          to="/"
+          style={{
+            color: "rgb(202, 0, 0)",
+            textDecoration: "none",
+            fontSize: "1.8rem",
+            marginTop: "1rem",
+          }}
+        >
+          Go back to home
+        </Link>
+      </main>
+    </>
   );
 };
 
